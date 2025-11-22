533785c60835516d9c9195e8661b10c7d614d766
Mahdi Nasserzadeh <nasserzadeh@hotmail.ch>
2025-11-22 21:51:14 +0100

    Add logging for Riot API rate limit headers

--- src/main/kotlin/com/pride/summoner_searcher_api/config/RestClientConfig.kt
+++ src/main/kotlin/com/pride/summoner_searcher_api/config/RestClientConfig.kt
@@ -26,8 +26,26 @@ class RestClientConfig {
      */
     @Bean
     fun riotRestClient(): RestClient {
+        val logger = org.slf4j.LoggerFactory.getLogger(RestClientConfig::class.java)
+        
         return RestClient.builder()
             .defaultHeader("X-Riot-Token", riotApiKey)
+            .requestInterceptor { request, body, execution ->
+                val response = execution.execute(request, body)
+                
+                val appCount = response.headers["X-App-Rate-Limit-Count"]?.firstOrNull()
+                val methodCount = response.headers["X-Method-Rate-Limit-Count"]?.firstOrNull()
+                
+                if (appCount != null) {
+                    // Log at INFO so we can see it in production logs easily
+                    logger.info("Riot Rate Limit: App Count: [{}]", appCount)
+                }
+                if (methodCount != null) {
+                    logger.debug("Riot Rate Limit: Method Count: [{}]", methodCount)
+                }
+                
+                response
+            }
             .build()
     }
 }
